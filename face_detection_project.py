516738d65bf78a2c3a586a9c9d6ce72914fcf2db
UmmeFarda <nummefarda2002@gmail.com>
2024-05-25 17:59:23 +0530

    face_detection_project.py

---
+++ face_detection_project.py
@@ -0,0 +1,66 @@
+!pip install opencv-python-headless
+!pip install numpy
+!pip install matplotlib
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+from IPython.display import display, Javascript
+from google.colab.output import eval_js
+from base64 import b64decode
+
+def take_photo(filename='photo.jpg', quality=0.8):
+    js = Javascript('''
+        async function takePhoto(quality) {
+            const div = document.createElement('div');
+            const capture = document.createElement('button');
+            capture.textContent = 'Capture';
+            div.appendChild(capture);
+
+            const video = document.createElement('video');
+            video.style.display = 'block';
+            const stream = await navigator.mediaDevices.getUserMedia({video: true});
+
+            document.body.appendChild(div);
+            div.appendChild(video);
+            video.srcObject = stream;
+            await video.play();
+
+            // Resize the output to fit the video element.
+            google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+
+            // Wait for Capture to be clicked.
+            await new Promise((resolve) => capture.onclick = resolve);
+
+            const canvas = document.createElement('canvas');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0);
+            stream.getVideoTracks()[0].stop();
+            div.remove();
+            return canvas.toDataURL('image/jpeg', quality);
+        }
+    ''')
+    display(js)
+    data = eval_js('takePhoto({})'.format(quality))
+    binary = b64decode(data.split(',')[1])
+    with open(filename, 'wb') as f:
+        f.write(binary)
+    return filename
+def detect_faces(filename):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    img = cv2.imread(filename)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.axis('off')
+    plt.show()
+try:
+    filename = take_photo()
+    print('Saved to {}'.format(filename))
+    detect_faces(filename)
+except Exception as err:
+    print(str(err))
